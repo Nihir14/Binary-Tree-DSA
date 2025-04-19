a1fac005894d3a961758d60d21d480c391332d46
Nihir14 <nihirverma14@gmail.com>
2025-04-19 11:56:10 +0530

    update

---
+++ CustomBinaryTree.java
@@ -0,0 +1,12 @@
+public class CustomBinaryTree {
+    private class Node {
+        int value;
+        Node left, right;
+
+        Node(int value) {
+            this.value = value;
+            left = right = null;
+        }
+    }
+}
+
